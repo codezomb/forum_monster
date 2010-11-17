43f8d35ce6522cefc200b6d0f3ff66e303020ad7
Mike Reeves <mike.reeves@atomos13.com>
2010-11-17 00:03:25 -0700

    Cleaned up files

--- app/models/community/topic.rb
+++
@@ -1,2 +0,0 @@
-class Community::Topic < ActiveRecord::Base
-end
--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do |map|
-  resources :forums, :controller => 'community/forums' do
-    resources :topics, :controller => 'community/topics', :except => :index do
-      resources :posts, :controller => 'community/posts', :except => [:index, :show]
-    end
-  end
-end
